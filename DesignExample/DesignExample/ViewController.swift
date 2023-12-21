4c625dcae874c7af9c85ef42407dfa6c41272bf0
İrfan Behic Tırpancı <irfanbehic@gmail.com>
2023-12-21 16:18:57 +0300

    Add Navigation Controller and Header

--- DesignExample/DesignExample/ViewController.swift
+++ DesignExample/DesignExample/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  DesignExample
+//  DesignProject
 //
-//  Created by İrfan Behic Tırpancı on 20.12.2023.
+//  Created by İrfan Behic Tırpancı on 12.12.2023.
 //
 
 import UIKit
@@ -11,9 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = "Mental Health"
+        
+        let appearence = UINavigationBarAppearance()
+        appearence.backgroundColor = UIColor(named: "backgroundColor")
+        appearence.titleTextAttributes = [.foregroundColor: UIColor(named: "textColor3")!,
+                                          .font: UIFont(name: "Avenir 85 Heavy", size: 22)! ]
+        navigationController?.navigationBar.barStyle = .black
+        
+        navigationController?.navigationBar.standardAppearance = appearence
+        navigationController?.navigationBar.compactAppearance  = appearence
+        navigationController?.navigationBar.scrollEdgeAppearance = appearence
+
+
+
     }
 
 
 }
+
 
