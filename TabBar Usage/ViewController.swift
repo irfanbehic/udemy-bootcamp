2812cec07c1b707d43c3ba1869d4695073ffeca2
İrfan Behic Tırpancı <irfanbehic@gmail.com>
2023-12-27 17:27:51 +0300

    Initial Commit

---
+++ TabBar Usage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabBar Usage
+//
+//  Created by İrfan Behic Tırpancı on 27.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
