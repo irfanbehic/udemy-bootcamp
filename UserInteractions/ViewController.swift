af126defa3c5181a3203348ff25ab7898df95eb4
İrfan Behic Tırpancı <irfanbehic@gmail.com>
2024-01-02 19:12:51 +0300

    Initial Commit

---
+++ UserInteractions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserInteractions
+//
+//  Created by İrfan Behic Tırpancı on 2.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
