c7da5bd9e4be0b02f69b41cdfee626b213f6c203
İrfan Behic Tırpancı <irfanbehic@gmail.com>
2024-01-04 22:56:43 +0300

    Creating TextField - ImageView - Switch

--- WidgetsUsage/ViewController.swift
+++ WidgetsUsage/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelResult: UILabel!
+    
+    @IBOutlet weak var mSwitch: UISwitch!
+    
+    @IBOutlet weak var textFieldInput: UITextField!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+//    butona tıkladığımızda labeldan veriyi alıp textfielda aktarmasını sağlayacağız.
+    @IBAction func buttonReceivedData(_ sender: Any) {
+        if let receivedData = textFieldInput.text {
+            
+            labelResult.text = receivedData
+        }
+        
+    }
+    
+    @IBAction func buttonRawOn(_ sender: Any) {
+        imageView.image = UIImage(named: "raw-on")
+    }
+    
+    @IBAction func buttonRawOff(_ sender: Any) {
+        imageView.image = UIImage(named: "raw-off")
+    }
+    
+//    switche tıkladığımız zaman özelliğini(true-false) sonuç olarak bize versin
+    
+    @IBAction func switchChange(_ sender: UISwitch) {
+        if sender.isOn {
+            print("Switch : ON")
+        }
+        else {
+            print("Switch : OFF")
+        }
+    }
+//    Show butonuna bastığımızda switchin son durumunu söylemesini istiyoruz.
+    @IBAction func buttonShow(_ sender: Any) {
+        
+        print("Switch Status : \(mSwitch.isOn)")
+    }
+    
+    
+    
 }
 
