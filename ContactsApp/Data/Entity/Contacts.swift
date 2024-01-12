293063d4f58ebbc78ef37c006e561faec01b05b5
İrfan Behic Tırpancı <irfanbehic@gmail.com>
2024-01-12 17:47:53 +0300

    Building Foundation Structures

---
+++ ContactsApp/Data/Entity/Contacts.swift
@@ -0,0 +1,8 @@
+//
+//  Contacts.swift
+//  ContactsApp
+//
+//  Created by İrfan Behic Tırpancı on 12.01.2024.
+//
+
+import Foundation
---
+++ ContactsApp/UI/Views/ContactsDetail.swift
@@ -0,0 +1,29 @@
+//
+//  ContactsDetail.swift
+//  ContactsApp
+//
+//  Created by İrfan Behic Tırpancı on 12.01.2024.
+//
+
+import UIKit
+
+class ContactsDetail: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
