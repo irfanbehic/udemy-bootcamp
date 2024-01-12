d4eba11004dc1df32ebada0f52f2aa076214471a
İrfan Behic Tırpancı <irfanbehic@gmail.com>
2024-01-12 19:24:17 +0300

    Adding TableView and Functions

---
+++ ContactsApp/UI/TableView/ContactsCell.swift
@@ -0,0 +1,23 @@
+//
+//  ContactsCell.swift
+//  ContactsApp
+//
+//  Created by İrfan Behic Tırpancı on 12.01.2024.
+//
+
+import UIKit
+
+class ContactsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
