bf58bedda593f4af91903dc1083aadbfca52bee9
İrfan Behic Tırpancı <irfanbehic@gmail.com>
2024-01-18 19:25:03 +0300

    Initial Commit

---
+++ Test/MainPage.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by İrfan Behic Tırpancı on 18.01.2024.
+//
+
+import UIKit
+
+class MainPage: UIViewController {
+
+    @IBOutlet weak var mainLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func mainCalcButton(_ sender: Any) {
+    }
+}
+
