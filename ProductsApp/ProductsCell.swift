609baf29cfbc748b0e360eb9d1b909cc348599e8
İrfan Behic Tırpancı <irfanbehic@gmail.com>
2024-01-31 17:59:15 +0300

    Finishing Project

---
+++ ProductsApp/ProductsCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductsCell.swift
+//  ProductsApp
+//
+//  Created by İrfan Behic Tırpancı on 31.01.2024.
+//
+
+import UIKit
+
+class ProductsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
